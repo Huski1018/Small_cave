1dfeee2d5d48e9ee1895881a761198e97d408715
LCCZK <43646803+LCCZK@users.noreply.github.com>
2021-09-05 17:29:10 +0800

    Update Q_learning_Main.py

--- Q_learning_Main.py
+++ Q_learning_Main.py
@@ -18,7 +18,9 @@ for j in range (0, len(map.Qtable)):
     #epsilon = 9999999999999
     #decay = 0.9
     for i in range(timesoftranning):
+        #fetch Q-table according to gold state
         k=tuple(map.whichq(j))
+        
         Goldlist,accumulate,Done,location,state=map.reset(j)
         while Done==False:
             #if tuple((map.x_c,map.y_c))==(1,7):
@@ -28,19 +30,20 @@ for j in range (0, len(map.Qtable)):
             #else:
             direction=map.randomdirection()#add logic between random and choose max later.
 
-            
+            #the movement
             Done,R_P,nextstate,accumulate,state=map.move(direction,accumulate)
-
+            
+            #check the gold avilibility
             Goldstate=tuple(map.Goldlist.values())
-
+            
+            #Ballmen's
             k[int(state)][direction] = R_P + gamma * max(k[nextstate])
             
 
 
             state=nextstate
-        #epsilon = decay*epsilon
-    
-print(map.Qtable)
+        #epsilon -= decay*epsilon
+
 
     
 
